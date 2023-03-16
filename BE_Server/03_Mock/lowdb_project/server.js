e8d14ccccb71d6227efdee3f07ac1376d65001c1
Fahim <fahim.ahmadi@digitalcareerinstitute.org>
2023-03-16 10:06:58 +0100

    update lowdb

--- BE_Server/03_Mock/lowdb_project/server.js
+++ BE_Server/03_Mock/lowdb_project/server.js
@@ -19,7 +19,7 @@ db.data ||= { users: [] }     // db.data = db.data || {users: []}
 //define a route to get all users
 app.get('/users', (req, res) => { 
     console.log(db.data)
-    res.send(db.data)
+    res.send(db.data.users)
  })
 
 
@@ -28,20 +28,35 @@ app.post('/users', async(req, res) => {
     const newUser = req.body;
     db.data.users.push(newUser);
     await db.write();
-    res.send(db.data)
+    res.send(db.data.users)
 })
  
 //delete a user
 app.delete('/users', async(req, res) => { 
     db.data.users = db.data.users.filter(u => u.id != req.body.id);
     await db.write();
-
     res.send(db.data);
  })
 
 
  //update a user
-//app.put()
+app.patch('/users/:userid', async(req, res) => { 
+    //to read the parameter userid
+    let userid = req.params.userid;
+    //loop on users array 
+    //find the user with given userid
+    //update it
+    db.data.users = db.data.users.map((user) => {
+        if (user.id == userid) {
+            user.name = req.body.name
+        }
+        return user
+    });
+
+    await db.write()
+
+    res.send(db.data.users)
+ })
 
 
 
