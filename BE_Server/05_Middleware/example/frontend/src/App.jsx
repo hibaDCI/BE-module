526c3a44815f7eee78ea031ea98f067f7b56b40b
Fahim <fahim.ahmadi@digitalcareerinstitute.org>
2023-03-20 15:52:47 +0100

    middlewares

--- BE_Server/05_Middleware/example/frontend/src/App.jsx
+++ BE_Server/05_Middleware/example/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
 
 function App() {
   const [user, setUser] = useState({});
@@ -26,29 +27,35 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <form className="form" onChange={changeHandler} onSubmit={submitHandler}>
-        <div><label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" className="form-controller" />
+    <div className="w-100 row border border-secondary rounded p-5">
+      <h1 className='display-5 fw-bold mb-4 mx-auto'>Register Form</h1>
+      <form  onChange={changeHandler} onSubmit={submitHandler}>
+        <div className='mb-3 d-flex flex-column'>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" className="form-controller p-1 " />
         </div>
 
-        <div><label htmlFor="lname">LastName</label>
-        <input type="text" name="lname" id="lname" className="form-controller" />
+        <div className='mb-3 d-flex flex-column'>
+          <label htmlFor="lname">LastName</label>
+          <input type="text" name="lname" id="lname" className="form-controller p-1 " />
         </div>
 
-        <div><label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" className="form-controller" />
+        <div className='mb-3 d-flex flex-column'>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" className="form-controller p-1 " />
         </div>
 
-        <div><label htmlFor="pass">Password</label>
-        <input type="text" name="pass" id="pass" className="form-controller" />
+        <div className='mb-3 d-flex flex-column'>
+          <label htmlFor="pass">Password</label>
+          <input type="text" name="pass" id="pass" className="form-controller p-1 " />
         </div>
 
-        <div><label htmlFor="type">Type</label>
-        <input type="text" name="type" id="type" className="form-controller" />
+        <div className='mb-3 d-flex flex-column'>
+          <label htmlFor="type">Type</label>
+          <input type="text" name="type" id="type" className="form-controller p-1 " />
         </div>
 
-        <input type="submit" value="Register" className='btn btn-primary' />
+        <input type="submit" value="Register" className='w-100 btn btn-outline-primary mt-4' />
 
       </form>
     </div>
