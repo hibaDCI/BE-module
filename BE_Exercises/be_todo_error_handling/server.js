9a299d5df5ce0ab2b966e09fec555784a35f19c5
Fahim <fahim.ahmadi@digitalcareerinstitute.org>
2023-03-26 19:11:00 +0200

    solution todoApp-Error_Handling

--- BE_Exercises/be_todo_error_handling/server.js
+++ BE_Exercises/be_todo_error_handling/server.js
@@ -2,16 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { Low } from 'lowdb';
+import dotenv from 'dotenv';
 import { JSONFile } from 'lowdb/node';
 import { userRouter } from './routers/user.router.js';
 import { todoRouter } from './routers/todos.router.js';
 import { genericErrHandler, noRoute } from './middlewares/err.handler.js';
 
+//set environment variables
+process.NODE_ENV === 'production'
+    ? dotenv.config({ path: './config/production.env' })
+    : dotenv.config({ path: './config/development.env' });
 
 //create app
 const app = express();
 
-
+console.log(process.NODE_ENV==='production'?'production':"development");
 //database
 const adapter = new JSONFile('db.json');
 export const db = new Low(adapter);
@@ -34,6 +39,7 @@ app.use('/todos', todoRouter);
 app.use(noRoute);
 app.use(genericErrHandler);
 
+console.log(`Email: ${process.env.EMAIL}`);
 //port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, console.log(`Server is up on port: ${port} 👻`));
