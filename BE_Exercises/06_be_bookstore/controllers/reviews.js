2698a54bf97d1c153b7a9a6ccf4f87d76cbaf638
Fahim <fahim.ahmadi@digitalcareerinstitute.org>
2023-04-18 11:08:34 +0200

    exercise-6-solution

--- BE_Exercises/06_be_bookstore/controllers/reviews.js
+++ BE_Exercises/06_be_bookstore/controllers/reviews.js
@@ -1,10 +1,15 @@
 import { Book } from "../models/books.js";
 import { Review } from "../models/reviews.js";
+import createError from 'http-errors';
 
 //get all reviews
 export const getAllReviews = async (req, res, next) => {
   try {
-    
+    const reviews = await Review.find({});
+    res.status(200).json({
+      message: "list of all reviews",
+      reviews,
+    });
   } catch (error) {
     next(error);
   }
@@ -13,6 +18,24 @@ export const getAllReviews = async (req, res, next) => {
 //add new review for a specific book using bid
 export const addNewReview = async (req, res, next) => {
   try {
+    const bookid = req.params.bid;
+    const { username, text, date } = req.body;
+
+    if (!username || !text) {
+      throw createError(400, 'some of required fields are missing!')
+    }
+
+    const book = await Book.findById(bookid);
+    if (!book) {
+      throw createError.NotFound('There is no book with given bookid!');
+    }
+    
+    const newReview = await Review.create({ username, text, date, book: book._id });
+    res.status(201).json({
+      message: 'new review added!',
+      review: newReview
+    })
+
   } catch (error) {
     next(error);
   }
@@ -21,6 +44,16 @@ export const addNewReview = async (req, res, next) => {
 //get a single review by using review id
 export const getReviewById = async (req, res, next) => {
   try {
+    const { rid } = req.params;
+    const review = await Review.findById(rid);
+    if (!review) {
+      throw createError.NotFound('There is no review for given reviewid!');
+    }
+
+    res.status(200).json({
+      message: "get single review by reviewid",
+      review
+    })
   } catch (error) {
     next(error);
   }
@@ -29,6 +62,18 @@ export const getReviewById = async (req, res, next) => {
 //update a review by id
 export const updateReviewById = async (req, res, next) => {
   try {
+    const { rid } = req.params;
+    const review = await Review.findById(rid);
+    if (!review) {
+      throw createError.NotFound("There is no review for given reviewid!");
+    }
+    
+    const updatedReivew = await Review.findByIdAndUpdate(rid, { ...req.body }, { runValidators: true, new: true });
+    res.status(200).json({
+      message: 'document updated successfully!',
+      updatedReivew
+    })
+
   } catch (error) {
     next(error);
   }
@@ -37,6 +82,19 @@ export const updateReviewById = async (req, res, next) => {
 //delete a review by id
 export const deleteReviewById = async (req, res, next) => {
   try {
+
+    const { rid } = req.params;
+    const review = await Review.findById(rid);
+    if (!review) {
+      throw createError.NotFound("There is no review for given reviewid!");
+    }
+
+    const deleted_review = await Review.findByIdAndRemove(rid);
+    res.status(200).json({
+      message: "review is removed",
+      deleted_review
+    })
+
   } catch (error) {
     next(error);
   }
