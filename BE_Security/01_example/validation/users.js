6d6e67d4c0d8bf20e18eb86c9e23b5d38350ee91
Fahim <fahim.ahmadi@digitalcareerinstitute.org>
2023-04-24 11:39:54 +0200

    update online-shop

--- BE_Security/01_example/validation/users.js
+++ BE_Security/01_example/validation/users.js
@@ -1,8 +1,50 @@
-import { check } from 'express-validator';
+import { body } from "express-validator";
+import { User } from "../models/users.js";
+
+//custom validation method to find duplicate emails
+const checkDuplicateEmail = async (email) => {
+  const user = await User.findOne({ email });
+
+  if (user) {
+    return Promise.reject("This email already in use!");
+  }
+};
 
 export const usersValidations = [
-    check("email")
-            .isEmail().withMessage("Please provide a valid email format!"),
-    check("name")
-            .isAlpha().withMessage("Please use alphabets only for Name field!"),
+  body("name")
+    .notEmpty()
+    .withMessage("Name field is a required field!")
+    .matches(/^[A-Za-z\s]{3,}$/)
+    .withMessage("Please use Alphabets and space only for Name field!"),
+
+  body("birthdate")
+    .notEmpty()
+    .withMessage("Birthdate field is a required field!")
+    .isDate()
+    .withMessage("Please provide a date for Birthdate field!"),
+
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required field!")
+    .isEmail()
+    .withMessage("Invalid Email!")
+    .custom(checkDuplicateEmail),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required!")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/)
+    .withMessage("Invalid Password!"),
+
+  body("address.city").notEmpty().withMessage("City is a required field!"),
+
+  body("address.zipcode")
+    .notEmpty()
+    .withMessage("Zipcode is a required field!"),
+
+  body("address.street").notEmpty().withMessage("Street is a required field!"),
+
+  body("address.building")
+    .notEmpty()
+    .withMessage("Building is a required field!"),
 ];
